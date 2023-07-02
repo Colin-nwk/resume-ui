2deccf436b6b01cf7862e1cd091575f0ac160610
Collins Nwoko C <collinsnwoko05@gmail.com>
2023-07-02 13:34:27 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    maxWidth: {
+      container: "1440px",
+      contentContainer: "1140px",
+      containerSmall: "1024px",
+      containerXs: "768px",
+    },
+    extend: {
+      screens: {
+        xs: "320px",
+        sm: "375px",
+        sml: "500px",
+        md: "667px",
+        mdl: "768px",
+        lg: "960px",
+        lgl: "1024px",
+        xl: "1280px",
+      },
+      boxShadow: {
+        navbarShadow: "0 10px 30px -10px rgba(2,12,27,0.7",
+      },
+      colors: {
+        theme: {
+          900: "#050A30",
+          700: "#000C66",
+          500: "#0000FF",
+          300: "#7EC8E3",
+        },
+      },
+      fontFamily: {
+        bodyFont: ["Montserrat", "sans-serif"],
+        titleFont: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
